5e68e0ccc63e0ade08be24c2a79221e0bc333f57
Michael Perez <mperez6473@gmail.com>
2020-04-24 11:06:05 -0500

    I have the colors set on BubblePage.js. setColorList to res.data

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,41 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, {
+    useState,
+    useEffect,
+    Component,
+    componentDidMount,
+} from 'react';
+import axios from 'axios';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
+import { authWithAxios } from '../utils/authWithAxios';
 
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+function BubblePage() {
+    const [colorList, setColorList] = useState([]);
+    console.log(colorList);
+    // fetch your colors data from the server when the component mounts
+    // set that data to the colorList state property
+    // componentDidMount = () => {
+    //     this.getData();
+    // };
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
-};
+    const getData = () => {
+        authWithAxios()
+            .get('/api/colors')
+            .then((res) => {
+                console.log(res.data);
+                setColorList(res.data);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    return (
+        <>
+            {getData()}
+            <ColorList colors={colorList} updateColors={setColorList} />
+            <Bubbles colors={colorList} />
+        </>
+    );
+}
 
 export default BubblePage;
