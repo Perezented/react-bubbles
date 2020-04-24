9b677abcb15eeadbb57e35abcb93273f71752aff
Michael Perez <mperez6473@gmail.com>
2020-04-24 11:15:16 -0500

    BubblePage.js is broken of some sorts. Does not like my componentDidMount and then it does not like the useState line.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,41 +1,20 @@
-import React, {
-    useState,
-    useEffect,
-    Component,
-    componentDidMount,
-} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Bubbles from './Bubbles';
 import ColorList from './ColorList';
-import { authWithAxios } from '../utils/authWithAxios';
 
-function BubblePage() {
+const BubblePage = () => {
     const [colorList, setColorList] = useState([]);
-    console.log(colorList);
     // fetch your colors data from the server when the component mounts
     // set that data to the colorList state property
-    // componentDidMount = () => {
-    //     this.getData();
-    // };
-
-    const getData = () => {
-        authWithAxios()
-            .get('/api/colors')
-            .then((res) => {
-                console.log(res.data);
-                setColorList(res.data);
-            })
-            .catch((err) => console.log(err));
-    };
 
     return (
         <>
-            {getData()}
             <ColorList colors={colorList} updateColors={setColorList} />
             <Bubbles colors={colorList} />
         </>
     );
-}
+};
 
 export default BubblePage;
